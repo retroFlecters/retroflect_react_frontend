acb69fbb4fc97e3c05ab9534cae207e49bcebcf6
Anna Cotugno <annaeuridice@gmail.com>
2020-01-09 17:40:13 +0000

    started to work on api link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Layout from '../components/Layout'
 import Carousel from 'react-bootstrap/Carousel'
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => (
+const Index = (props) => (
     <Layout>
     <div className="app">
 
@@ -9,8 +11,30 @@ const Index = () => (
             <h2>Welcome to retroFlect</h2>
         </header>
 
+        <ul>
+          {props.posts.map(post => (
+            <li key={show.id}>
+              <Link href="/p/[id]" as={`/p/${show.id}`}>
+                <a>{show.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+
     </div>
     </Layout>
 )
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://retroflect-core.herokuapp.com/api/entries');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    posts: data.map(entry => entry.post)
+  };
+};
 
 export default Index;
